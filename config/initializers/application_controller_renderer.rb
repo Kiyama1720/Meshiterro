ec794533560b6be75da4eb5e423ce0d9e8c36796
Haruki Kiyama <aladdin.magi101@gmail.com>
2021-04-10 10:39:58 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e691a097e5bc01fa3b99e73e1ad2f78901368bf41d24c514ba6d9ed9a7208415d082cbdabde6255ad9b79f9b596cff38a4b7b7dca135afb926f08e87aebe9e6d'
